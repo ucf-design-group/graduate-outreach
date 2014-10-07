cf3a3c58a8ba5dc5e64846bf6cffd4613fae40a9
chris-judge <chris_judge941@yahoo.com>
2014-10-07 10:39:50 -0400

    Restructure header php.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width" />
 		<meta http-equiv="X-UA-Compatible" content="chrome=1" />
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<title><?php wp_title( '|', true, 'right' ); ?> UCF Graduate Outreach</title>
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 		<![endif]-->
@@ -22,11 +22,11 @@
 	<body <?php echo $body_class; ?>>
 		<div class="page">
 			<header>
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/gologo.png" alt="">
 				<nav class="main-menu full">
 					<div class="screen-reader-text skip-link"><a href="#UPDATE ME" title="Skip to content">Skip to content</a></div>
 					<div class="compact-menu">
 						<a href="#" class="menu-toggle">Menu</a>
-						<?php get_search_form(); ?>
 					</div>
 					<ul>
 <?php
@@ -49,11 +49,6 @@
 						<li<?php echo $nav_li_class; ?>><a href="<?php echo $link; ?>"><?php echo $name; ?></a></li>
 <?php 							}
 							} ?>
-						<li>
-							<form method="get" id="searchform" class="searchform" action="http://localhost/wp/" role="search">
-								<input type="search" class="field" name="s" value="" id="s" placeholder="Search &#133;" />
-							</form>
-						</li>
 					</ul>
 				</nav>
 			</header>
