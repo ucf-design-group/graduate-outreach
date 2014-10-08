eb0eec1a8350c98fe2c76cab4e848c85d3d56acb
chris-judge <chris_judge941@yahoo.com>
2014-10-08 14:16:53 -0400

    Change street team image.

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ get_header(); ?>
 				</div>
 				<div class="streetteamAdWrap">
 					<div class="streetteamAd">
-						<img src="http://placekitten.com/325/325" alt="">
+						<img src="http://placehold.it/400x400" alt="">
 						<div class="streetteamAdDescription">
 							<h1>Four loko post-ironic actually</h1>
 							<p>Four loko post-ironic actually, Bushwick viral PBR&amp;B distillery butcher. American Apparel iPhone Austin, next level chia fingerstache asymmetrical actually sriracha XOXO VHS. Forage hoodie art party authentic artisan locavore tattooed. Austin stumptown before they sold out, irony whatever locavore butcher crucifix letterpress Schlitz sartorial normcore brunch. Semiotics gluten-free Vice shabby chic plaid Odd Future. Synth Kickstarter keytar lomo master cleanse. Church-key gluten-free irony, art party four loko cred lomo crucifix next level disrupt hashtag.</p>
