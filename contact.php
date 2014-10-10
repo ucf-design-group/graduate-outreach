45832454f3da17836576c8b5f0e6ea8ec0eb8009
John Costallos <jpcostallos@gmail.com>
2014-10-10 16:29:11 -0400

    add contact page styles

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@ get_header(); ?>
 				</div>
 
 					<section class="exec-board">
-						<h2>Knight-Thon Executive Board</h2>
+						<h2>Graduate Outreach Executive Board</h2>
 <?php
 						$leaderLoop = new WP_QUERY(array('post_type' => 'exec-board', 'posts_per_page' => -1, 'orderby' =>'meta_value', 'order' => 'ASC', 'meta_key' => 'leader-form-order'));
 						while ($leaderLoop->have_posts()) {
@@ -21,13 +21,15 @@ get_header(); ?>
 							$image = get_the_post_thumbnail($post->ID, 'thumbnail');
 							$position = get_post_meta($post->ID, 'leader-form-position', true);
 							$email = get_post_meta($post->ID, 'leader-form-email', true);
+							$image_url = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'full');
+
 ?>	
-						<article class="leader">
-							<h3><?php echo $title; ?></h3>
-							<?php echo $image; ?>
-							<p><?php echo $position; ?></p>
-							<p><?php echo $content; ?></p>
-							<a class="email" href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+						<article class="leader" style="background-image: url('<?php echo $image_url[0]; ?>') ">
+							<div class="leaderinfo">
+								<h3><?php echo $title; ?></h3>
+								<p><?php echo $position; ?></p>
+								<a class="email" href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+							</div>
 						</article>
 <?php 				}
 ?>
