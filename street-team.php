33ec071c7992b3202dc0a6e202fd50b891756d59
chris-judge <chris_judge941@yahoo.com>
2014-10-29 12:49:22 -0400

    Remove bottom section.

--- street-team.php
+++ street-team.php
@@ -2,8 +2,14 @@
 get_header(); ?>
 
 			<div class="content-area">
-				<div class="streetTeamSection">
-					
+				<div class="streetSectionWrap">
+					<div class="streetSection">
+						<img src="http://placehold.it/400x400" alt="">
+						<div class="streetSectionDescription">
+							<h1>Four loko post-ironic actually</h1>
+							<p>Four loko post-ironic actually, Bushwick viral PBR&amp;B distillery butcher. American Apparel iPhone Austin, next level chia fingerstache asymmetrical actually sriracha XOXO VHS. Forage hoodie art party authentic artisan locavore tattooed. Austin stumptown before they sold out, irony whatever locavore butcher crucifix letterpress Schlitz sartorial normcore brunch. Semiotics gluten-free Vice shabby chic plaid Odd Future. Synth Kickstarter keytar lomo master cleanse. Church-key gluten-free irony, art party four loko cred lomo crucifix next level disrupt hashtag.</p>
+						</div>
+					</div>
 				</div>
 				<div class="main">
 					<?php
