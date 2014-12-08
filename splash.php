8b7079af7c7889e0dfc5921b5f70c72467fbcabb
chris-judge <chris_judge941@yahoo.com>
2014-12-08 15:38:20 -0500

    Create splash template.

---
+++ splash.php
@@ -0,0 +1,37 @@
+<?php /* Template Name: Splash */ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width" />
+		<meta http-equiv="X-UA-Compatible" content="chrome=1" />
+		<title><?php wp_title( '|', true, 'right' ); ?> UCF Graduate Outreach</title>
+		<!--[if lt IE 9]>
+		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+		<![endif]-->
+		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+	</head>
+<?php
+	global $post;
+	if ($post->post_type == "page")
+		$body_class = 'class="page-' . $post->post_name . '"';
+	else if ($post->post_type != "")
+		$body_class = ($post->post_type != 'post') ? 'class="post-' . $post->post_type . '"' : 'class="post-default"';
+	else
+		$body_class = "";
+?>
+	<body <?php echo $body_class; ?>>
+		<div class="page">
+<!-- HEADER END -->
+			<div class="content-area">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/gologo2.png" alt="">
+				<h1>Sorry!</h1>
+				<h2>This site is currently under construction.</h2>
+				<div class="main">
+				</div>
+				<!-- <aside> -->
+					<!-- OPTIONAL -->
+				<!-- </aside> -->
+			</div>
+
+<?php get_footer(); ?>
